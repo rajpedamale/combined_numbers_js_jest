f4da01e3406efb6f1812ec920c4ca10c363f22fb
lD0Sx3 <lD0Sx3@cyber-dojo.org>
2023-02-13 07:48:46 +0000

    5 ran tests, predicted red, got red

--- files/combined_number.test.js
+++ files/combined_number.test.js
@@ -10,4 +10,8 @@ describe('Combined Number', () => {
   it('should return a string of the number for a single number', () => {
     expect(combined_number([0])).toEqual('0');
   });
+
+  it('should return "42" for [2, 4]', () => {
+    expect(combined_number([2, 4])).toEqual('42');
+  });
 });

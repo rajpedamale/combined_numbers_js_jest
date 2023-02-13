ad63ce27430f1aa8b15cae5edbbbd2f75b34160d
lD0Sx3 <lD0Sx3@cyber-dojo.org>
2023-02-13 10:54:08 +0000

    15 ran tests, predicted green, got green

--- files/combined_number.js
+++ files/combined_number.js
@@ -3,9 +3,19 @@
 const sortRule = (a, b) => {
     const sa = a.toString();
     const sb = b.toString();
-    for (let num = 0; num < sa.length; num++) {
+    for (let num = 0; num < Math.max(sa.length, sb.length); num++) {
+        if (typeof sa[num] === 'undefined' && typeof sb[num] === 'undefined' ) return 0;
+        if (typeof sa[num] === 'undefined') {
+            const compareValue = Number(sb[num]) - Number(sa[0]);
+            return compareValue === 0 ? 1 : compareValue;
+        }
+        if (typeof sb[num] === 'undefined') {
+            const compareValue = Number(sb[0]) - Number(sa[num]);
+            return compareValue === 0 ? -1 : compareValue;
+        }
         if (sa[num] !== sb[num]) return Number(sb[num]) - Number(sa[num]);
     }
+    return 0;
 };
 
 function combined_number(numbers) {

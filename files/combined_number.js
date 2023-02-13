bcca7980665a2cdd334e0474ae842c4864b47b46
lD0Sx3 <lD0Sx3@cyber-dojo.org>
2023-02-13 07:47:42 +0000

    4 ran tests, predicted green, got green

--- files/combined_number.js
+++ files/combined_number.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function combined_number() {
-  return '';
+function combined_number(numbers) {
+  return numbers.map(number => number.toString()).join();
 }
 
 module.exports = combined_number;

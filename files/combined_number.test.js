6c69a4402c944f9ecbb05b7cde3587eb34a15a8d
lD0Sx3 <lD0Sx3@cyber-dojo.org>
2023-02-13 07:45:38 +0000

    3 ran tests, predicted red, got red

--- files/combined_number.test.js
+++ files/combined_number.test.js
@@ -4,6 +4,10 @@ const combined_number = require('./combined_number');
 
 describe('Combined Number', () => {
   it('should return a string', () => {
-    expect(typeof combined_number('')).toEqual('string');
+    expect(typeof combined_number([])).toEqual('string');
+  });
+
+  it('should return a string of the number for a single number', () => {
+    expect(combined_number([0])).toEqual('0');
   });
 });

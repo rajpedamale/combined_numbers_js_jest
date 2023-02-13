ca986e672bf949cc01197ca171e439bc612393c5
lD0Sx3 <lD0Sx3@cyber-dojo.org>
2023-02-13 08:06:29 +0000

    11 ran tests, predicted green, got red

--- files/combined_number.js
+++ files/combined_number.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const sortRule = (a, b) => b-a;
+const sortRule = (a, b) => {
+    const sa = a.toString();
+    const sb = b.toString();
+    for (let num = 0; num < sa.length; num++) {
+        if (sa[num] !== sb[num]) return Number(b) - Number(a);
+    }
+};
 
 function combined_number(numbers) {
     return numbers
